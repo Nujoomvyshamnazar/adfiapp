caec9cc8e5da74f5fba38111db7d5de852f8c5fc
nujoom nazar <nujoomvyshamnazar@gmail.com>
2019-06-27 18:32:47 +0400

    updated investor all transactions

--- adfiApp/resources/views/investor/pages/all-transactions.blade.php
+++ adfiApp/resources/views/investor/pages/all-transactions.blade.php
@@ -59,7 +59,7 @@
                                     <th class="numeric">Due Date</th>
                                     <th class="numeric">Status</th>
                                     <th class="numeric">Remark</th>
-
+<th class="numeric">Action</th>
                                   </tr>
                               </thead>
                               <tbody>
@@ -75,7 +75,7 @@
                                   <td>20-06-2019</td>
                                 <td>Buyer Approved</td>
                                   <td>No Remarks</td>
-
+      <td><button type="button" class="btn btn-info btn_sizes">View Invoice</button></td>
                                 </tr>
 
                               </tbody>
