15d775d74f36ab81214ce671bd4d31c78f386d96
nujoom nazar <nujoomvyshamnazar@gmail.com>
2019-06-27 17:05:31 +0400

    updated admin all transaction

--- adfiApp/resources/views/admin/pages/all-transactions.blade.php
+++ adfiApp/resources/views/admin/pages/all-transactions.blade.php
@@ -41,7 +41,7 @@
      </div>
   </div>
 
-                      <div class="table-scrollable">
+                      <div class="table-responsive" width="100%">
 
                               <table class="table table-striped table-bordered table-hover" id="table1">
                               <thead>
@@ -116,7 +116,7 @@
 <link rel="stylesheet" type="text/css" href="{{asset('assets/vendors/select2/css/select2.min.css')}}" />
 <link rel="stylesheet" type="text/css" href="{{asset('assets/vendors/select2/css/select2-bootstrap.css')}}" />
    <link href="{{asset('assets/css/pages/tables.css')}}" rel="stylesheet" type="text/css" />
-  <link rel="stylesheet" type="text/css" href="{{asset('assets/vendors/datetimepicker/css/bootstrap-datetimepicker.min.css')}}">
+
 
 
 
@@ -128,17 +128,13 @@
 <!-- end of global js -->
 <script type="text/javascript" src="{{asset('assets/vendors/datatables/js/jquery.dataTables.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/vendors/datatables/js/dataTables.bootstrap.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/vendors/datatables/js/dataTables.responsive.js')}}"></script>
+
 
 <script type="text/javascript" src="{{asset('assets/vendors/select2/js/select2.js')}}"></script>
 
 <script type="text/javascript" src="{{asset('assets/js/pages/table-advanced2.js')}}"></script>
 
-<script src="{{asset('assets/vendors/moment/js/moment.min.js')}}"></script>
- <script src="{{asset('assets/vendors/jasny-bootstrap/js/jasny-bootstrap.js')}}" type="text/javascript"></script>
- <script src="{{asset('assets/vendors/select2/js/select2.js')}}" type="text/javascript"></script>
- <script src="{{asset('assets/vendors/bootstrapwizard/jquery.bootstrap.wizard.js')}}" type="text/javascript"></script>
- <script src="{{asset('assets/vendors/datetimepicker/js/bootstrap-datetimepicker.min.js')}}" type="text/javascript"></script>
+
 <!-- to add bottons
 
 
@@ -158,21 +154,6 @@
 
 -->
 
-<script>
-
-$(document).ready(function() {
-
-    $("#dob").datetimepicker({
-        format: 'YYYY-MM-DD',
-        widgetPositioning:{
-            vertical:'bottom'
-        },
-        keepOpen:false,
-        useCurrent: false,
-        maxDate: new Date()
-    });
-});
-</script>
 
 
 
