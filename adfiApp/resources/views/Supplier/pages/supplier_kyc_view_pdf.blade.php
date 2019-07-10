9e48ecf6a789a73aed7cea385161ad71e2b782f3
nujoom nazar <nujoomvyshamnazar@gmail.com>
2019-07-10 10:12:59 +0400

    supplier kyc view updated

--- adfiApp/resources/views/Supplier/pages/supplier_kyc_view_pdf.blade.php
+++ adfiApp/resources/views/Supplier/pages/supplier_kyc_view_pdf.blade.php
@@ -88,88 +88,88 @@
 
 
 
-    <table class="table table-striped table-bordered" id="inline_edit">
-      <tr>
-      <h3>Entity Profiling:</h3>
-    </tr>
-<tr>
-<td><label class="control-label">Entity Name:</label></td>
-<td>Value</td>
-<td><label class="control-label">Entity Type:</label></td>
-<td>Value</td>
-</tr>
+       <table class="table table-striped table-bordered" id="inline_edit">
+       <tr>
+       <h3>Entity Profiling:</h3>
+       </tr>
+       <tr>
+       <td><label class="control-label">Entity Name:</label></td>
+       <td>Value</td>
+       <td><label class="control-label">Entity Type:</label></td>
+       <td>Value</td>
+       </tr>
 
-<tr>
-<td><label class="control-label">Nature of Business:</label></td>
-<td>Value</td>
-<td><label class="control-label">Industry:</label></td>
-<td>Value</td>
-</tr>
-
-
-<tr>
-<td><label class="control-label">Incorporation date:</label></td>
-<td>Value</td>
-<td><label class="control-label">Country of Operations:</label></td>
-<td>Value</td>
-</tr>
+       <tr>
+       <td><label class="control-label">Nature of Business:</label></td>
+       <td>Value</td>
+       <td><label class="control-label">Industry:</label></td>
+       <td>Value</td>
+       </tr>
 
 
-<tr>
-<td><label class="control-label">Country of Head office:</label></td>
-<td>Value</td>
-<td><label class="control-label">Company ID Type:</label></td>
-<td>Value</td>
-</tr>
-
-<tr>
-<td><label class="control-label">Company ID Number:</label></td>
-<td>Value</td>
-<td><label class="control-label">Company ID Date of First Issuance:</label></td>
-<td>Value</td>
-</tr>
-
-<tr>
-<td><label class="control-label">Company ID Date of Expiry:</label></td>
-<td>Value</td>
-<td><label class="control-label">Name of the Auditor:</label></td>
-<td>Value</td>
-</tr>
-
-<tr>
-<td><label class="control-label">Registered Addresss:</label></td>
-<td>Value</td>
-<td><label class="control-label">Mailing Adresss:</label></td>
-<td>Value</td>
-</tr>
-
-<tr>
-<td><label class="control-label">Contact Numbers:</label></td>
-<td>Value</td>
-<td><label class="control-label">Sources of Income / Other businesses:</label></td>
-<td>Value</td>
-</tr>
-
-<tr>
-<td><label class="control-label">Annual Turnover:</label></td>
-<td>Value</td>
-<td><label class="control-label">Sources of Income /Other businesses:</label></td>
-<td>Value</td>
-</tr>
-
-<tr>
-<td><label class="control-label">Group Company Details:</label></td>
-<td>Value</td>
-
-</tr>
-    </table>
+       <tr>
+       <td><label class="control-label">Incorporation date:</label></td>
+       <td>Value</td>
+       <td><label class="control-label">Country of Operations:</label></td>
+       <td>Value</td>
+       </tr>
 
 
-    <table class="table table-striped table-bordered" id="inline_edit">
-      <tr>
-      <h3>Board of Directors and Senior Management :</h3>
-    </tr>
-    <thead>
+       <tr>
+       <td><label class="control-label">Country of Head office:</label></td>
+       <td>Value</td>
+       <td><label class="control-label">Company ID Type:</label></td>
+       <td>Value</td>
+       </tr>
+
+       <tr>
+       <td><label class="control-label">Company ID Number:</label></td>
+       <td>Value</td>
+       <td><label class="control-label">Company ID Date of First Issuance:</label></td>
+       <td>Value</td>
+       </tr>
+
+       <tr>
+       <td><label class="control-label">Company ID Date of Expiry:</label></td>
+       <td>Value</td>
+       <td><label class="control-label">Name of the Auditor:</label></td>
+       <td>Value</td>
+       </tr>
+
+       <tr>
+       <td><label class="control-label">Registered Addresss:</label></td>
+       <td>Value</td>
+       <td><label class="control-label">Mailing Adresss:</label></td>
+       <td>Value</td>
+       </tr>
+
+       <tr>
+       <td><label class="control-label">Contact Numbers:</label></td>
+       <td>Value</td>
+       <td><label class="control-label">Sources of Income / Other businesses:</label></td>
+       <td>Value</td>
+       </tr>
+
+       <tr>
+       <td><label class="control-label">Annual Turnover:</label></td>
+       <td>Value</td>
+       <td><label class="control-label">Sources of Income /Other businesses:</label></td>
+       <td>Value</td>
+       </tr>
+
+       <tr>
+       <td><label class="control-label">Group Company Details:</label></td>
+       <td>Value</td>
+
+       </tr>
+       </table>
+
+
+       <table class="table table-striped table-bordered" id="inline_edit">
+       <tr>
+       <h3>Board of Directors and Senior Management :</h3>
+       </tr>
+       <thead>
         <tr>
             <th>Name </th>
             <th>Age</th>
@@ -177,8 +177,8 @@
             <th>Country of residence</th>
               <th>Years with the establishment</th>
         </tr>
-    </thead>
-    <tbody>
+       </thead>
+       <tbody>
         <tr>
             <td>value</td>
               <td>value</td>
@@ -188,7 +188,7 @@
         </tr>
 
 
-    </tbody>
+       </tbody>
 
         </table>
 
@@ -321,97 +321,103 @@
 
 
 
-    <table class="table table-striped table-bordered" id="inline_edit">
-      <tr>
-      <h3>Activity Profile:</h3>
-    </tr>
-    <thead>
+       <table class="table table-striped table-bordered" id="inline_edit">
+       <tr>
+       <h3>Activity Profile:</h3>
+       </tr>
+       <thead>
 
         <tr>
-            <th>  <h4>Anticipated Monthly activity:</h4>
-
-      </tr>
+            <th>  <h4>Anticipated Monthly activity:</h4></th>
+        <th> </th>
+       </tr>
         </thead>
-      <tr>
-          <td><label class="control-label">Invoices Generated/Month:</label></td>
+       <tr>
+          <td><label class="control-label">Invoices /Buyer   :</label></td>
             <td>Value</td>
-<td><label class="control-label">Invoices Generated/Month:</label></td>
-  <td>value</td>
-    </tr>
-      <tr>
-<td><label class="control-label">Average Credit Period/Supplier:</label></td>
-    <td>
-      Value
-  </td>
-</tr>
 
-<tr>
-<td><label class="control-label">Current Banks/ Sources of Finance:</label></td>
-<td>
-Value
-</td>
-</tr>
+       </tr>
+       <tr><td><label class="control-label">Invoices Generated/Month:</label></td>
+       <td>value</td></tr>
+       <tr>
+       <td><label class="control-label">Average Credit Period / Buyer</label></td>
+       <td>
+       Value
+       </td>
+       </tr>
 
-<tr>
-<td><label class="control-label">Bank Particulars with IBAN No:</label></td>
-<td>
-Value
-</td>
-</tr>
-
-<tr>
-<td><label class="control-label">Bank Name:</label></td>
-<td>
-Value
-</td>
-</tr>
-
-<tr>
-<td><label class="control-label">Branch:</label></td>
-<td>
-Value
-</td>
-</tr>
-    <tbody>
+       <tbody>
 
 
 
-    </tbody>
+       </tbody>
 
         </table>
 
 
         <table class="table table-striped table-bordered" id="inline_edit">
           <tr>
+          <h3>Bank Details:</h3>
+        </tr>
+        <thead>
+
+            <tr>
+                <th>Current Banks/ Sources of Finance:</th>
+                <th>Bank Particulars with IBAN No: </th>
+                <th>Bank Name:</th>
+                <th>Branch:</th>
+
+
+
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>value</td>
+                  <td>value</td>
+                  <td>value</td>
+                <td>value</td>
+
+
+            </tr>
+
+
+        </tbody>
+
+            </table>
+
+
+        <table class="table table-striped table-bordered" id="inline_edit">
+          <tr>
           <h3>Attachments:</h3>
         </tr>
-<tr>
+       <tr>
 
-<td><label class="col-sm-5 control-label">Valid Trade License/ Chamber of commerce:</label></td>
-<td>  <i class="livicon" data-name="download" data-size="25" data-loop="true" data-c="#000" data-hc="#000"></i></td>
-</tr>
+       <td><label class="col-sm-5 control-label">Valid Trade License/ Chamber of commerce:</label></td>
+       <td>  <i class="livicon" data-name="download" data-size="25" data-loop="true" data-c="#000" data-hc="#000"></i></td>
+       </tr>
 
-<tr>
+       <tr>
 
-<td><label class="col-sm-5 control-label">MOA/AOA with amendments:</label></td>
-<td>  <i class="livicon" data-name="download" data-size="25" data-loop="true" data-c="#000" data-hc="#000"></i></td>
-</tr>
+       <td><label class="col-sm-5 control-label">MOA/AOA with amendments:</label></td>
+       <td>  <i class="livicon" data-name="download" data-size="25" data-loop="true" data-c="#000" data-hc="#000"></i></td>
+       </tr>
 
-<tr>
+       <tr>
 
-<td><label class="col-sm-5 control-label">Passport Copies of the Promoters/ Signatories:</label></td>
-<td>  <i class="livicon" data-name="download" data-size="25" data-loop="true" data-c="#000" data-hc="#000"></i></td>
-</tr>
+       <td><label class="col-sm-5 control-label">Passport Copies of the Promoters/ Signatories:</label></td>
+       <td>  <i class="livicon" data-name="download" data-size="25" data-loop="true" data-c="#000" data-hc="#000"></i></td>
+       </tr>
 
-<tr>
+       <tr>
 
-<td><label class="col-sm-8 control-label">Cancelled Cheque image to validate the bank details for each of the banks :</label></td>
-<td>  <i class="livicon" data-name="download" data-size="25" data-loop="true" data-c="#000" data-hc="#000"></i></td>
-</tr>
+       <td><label class="col-sm-8 control-label">Cancelled Cheque image to validate the bank details for each of the banks :</label></td>
+       <td>  <i class="livicon" data-name="download" data-size="25" data-loop="true" data-c="#000" data-hc="#000"></i></td>
+       </tr>
 
-</table>
+       </table>
 
-  </section>
+       </section>
 
      </div>
    </div>
